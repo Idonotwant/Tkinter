c8b0b9912493adcefb9d0ca2d7a292f4a4ccdc70
Idonotwant <alps30318@gmail.com>
2022-04-25 17:54:22 +0800

    add grid to main

--- Drag/src/Drag.py
+++ Drag/src/Drag.py
@@ -6,9 +6,11 @@ Brief_Log:
 2022/04/23 Start
 Note: 
 '''
+from glob import escape
 import sys
 from tkinter import *
 from TkinterDnD2 import DND_FILES,TkinterDnD
+alignmode = "nswe"
 
 
 class Dragger(TkinterDnD.Tk):
@@ -17,25 +19,25 @@ class Dragger(TkinterDnD.Tk):
         self.wm_title('Drag')
         self.configure(background='white')
         self.wm_minsize(800,600)
-        self.wm_maxsize(1440,2800)
+        self.wm_maxsize(self.winfo_screenwidth(),self.winfo_screenheight())
         self.counter = 1
-        self.div_size = 200
-        self.div1 = Frame(self)
+        self.div1 = Frame(self,bg='green')
+        self.div1.grid(row=0,column=0,sticky=alignmode)
         self.labelCounter = Label(self.div1,text="0",fg="black")
-        self.labelCounter.grid(column=0,row=0)
+        self.labelCounter.grid(column=0,row=0,sticky=alignmode)
         self.fileList = Listbox(self.div1,selectmode=SINGLE,background="#ffe0d6")
-        self.fileList.grid(column=0,row=1)
+        self.fileList.grid(column=0,row=1,sticky=alignmode)
         self.fileList.drop_target_register(DND_FILES)
         self.fileList.dnd_bind("<<Drop>>",self.dropIn)
 
         self.div1.rowconfigure(0,weight=1)
         self.div1.rowconfigure(1,weight=5)
+        self.div1.columnconfigure(0,weight=1)
+        
         self.columnconfigure(0,weight=1)
-        self.columnconfigure(1,weight=6)
-        self.div1.grid(row=0,column=0)
-
-        print(self.size())
-
+        self.rowconfigure(0,weight=1)
+        
+        self.bind('<Escape>',self.leave)
         self.run()
         self.refresh_data()
         self.mainloop()
@@ -48,9 +50,10 @@ class Dragger(TkinterDnD.Tk):
             self.labelDragger.config(self,text=data,fg='black')
         self.after(1000,self.refresh_data)
     
-    def run(self):
+    def run(self,*args):
         pass
-    
+    def leave(self,*args):
+        sys.exit()
     def dropIn(self,event):
         self.fileList.insert("end",event.data)
 
